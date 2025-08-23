a4904155aee5602f0bbc35ecf7cd46a8bf3b8d50
LLuisFS <elreconect1@gmail.com>
2025-08-23 10:47:19 -0300

    CRUD dos contratos
    finalizado o crud dos contratos

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 from routes.auth_routes import auth_router
 from routes.company_routes import companies_router
 from routes.employee_routes import employees_router
+from routes.contract_routes import contracts_router
 
 
 app.include_router(auth_router)
 app.include_router(companies_router)
 app.include_router(employees_router)
+app.include_router(contracts_router)
---
+++ routes/contract_routes.py
@@ -0,0 +1,82 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+from models import User, Contract
+from schemas import ContractRead, ContractWithEmployees, ContractCreate
+from dependencies import take_session, get_current_user, get_current_admin_user
+
+
+contracts_router = APIRouter(prefix="/contracts", tags=["Contracts"])
+
+@contracts_router.get("/", response_model=List[ContractRead])
+async def get_contracts(
+        skip: int = 0,
+        limit: int = 100,
+        db: Session = Depends(take_session),
+        current_user: User = Depends(get_current_user)
+):
+    companies = db.query(Contract).offset(skip).limit(limit).all()
+    return companies
+
+@contracts_router.get("/{contract_id}", response_model=ContractRead)
+async def get_contract(
+        contract_id: int,
+        db: Session = Depends(take_session),
+        current_user: User = Depends(get_current_user)
+):
+    company = db.query(Contract).get(contract_id).first()
+    if not company:
+        raise HTTPException(status_code=404, detail="Contract not found")
+    return company
+
+@contracts_router.get("/{contract_id}/details", response_model=ContractWithEmployees)
+async def get_contract_details(
+        contract_id: int,
+        db: Session = Depends(take_session),
+        current_user: User = Depends(get_current_user)
+):
+    db_contract = db.query(Contract).get(contract_id).first()
+    if not db_contract:
+        raise HTTPException(status_code=404, detail="Contract not found")
+    return db_contract
+
+@contracts_router.post("/", response_model=ContractRead, status_code=201)
+async def create_contract(
+        contract: ContractCreate,
+        company_id: int,
+        db: Session = Depends(take_session),
+        current_user: User = Depends(get_current_admin_user)
+):
+    db_contract = Contract(contract_number=contract.contract_number, company_id=company_id)
+    db.add(db_contract)
+    db.commit()
+    db.refresh(db_contract)
+    return db_contract
+
+@contracts_router.put("/{contract_id}", response_model=ContractRead)
+async def update_contract(
+        contract_id: int,
+        contract: ContractCreate,
+        db: Session = Depends(take_session),
+        current_user: User = Depends(get_current_admin_user)
+):
+    db_contract = db.query(Contract).filter(Contract.id == contract_id).first()
+    if db_contract is None:
+        raise HTTPException(status_code=404, detail="Contract not found")
+    db_contract.contract_number = contract.contract_number
+    db.commit()
+    db.refresh(db_contract)
+    return db_contract
+
+@contracts_router.delete("/{contract_id}", status_code=204)
+async def delete_contract(
+        contract_id: int,
+        db: Session = Depends(take_session),
+        current_user: User = Depends(get_current_admin_user)
+):
+    db_contract = db.query(Contract).filter(Contract.id == contract_id).first()
+    if db_contract is None:
+        raise HTTPException(status_code=404, detail="Contract not found")
+    db.delete(db_contract)
+    db.commit()
+    return
